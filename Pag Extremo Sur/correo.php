82611aeeefe105c7278540c503e177b16d5ca38f
JulianCastro130 <84351657+JulianCastro130@users.noreply.github.com>
2021-07-20 17:30:52 -0300

    Add files via upload

---
+++ Pag Extremo Sur/correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatarios = 'cococastro619@gmail.com';
+
+    $nombre = $_POST['NOMBRE'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "Alguien que visitó la página te envío esto.";
+    $mensajeCompleto = $mensaje . "\nAntentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensaje, $telefono, $header);
+    echo "<script>alert('coreo enviado')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000) </script>";
+?>
